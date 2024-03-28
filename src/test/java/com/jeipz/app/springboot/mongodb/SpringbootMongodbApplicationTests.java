30d4770b5d6576caf8ac1a716847d8fce9f90895
John Paul Rosario <rosario.jeipz@gmail.com>
2024-03-28 11:41:02 +0900

    Initial Project Setup

---
+++ src/test/java/com/jeipz/app/springboot/mongodb/SpringbootMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeipz.app.springboot.mongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
